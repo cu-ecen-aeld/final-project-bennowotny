d874950050df233f4ea0c1d251d12c3e12f58474
Ben Nowotny <bennowotny65535@gmail.com>
2024-11-22 06:10:53 +0000

    wip: parameterized the kernel module, testing a couple different configurations for the PWM output
    Signed-off-by: Ben Nowotny <bennowotny65535@gmail.com>

--- kernelspace/rgb-led/generate-intellisense.sh
+++ kernelspace/rgb-led/generate-intellisense.sh
@@ -8,7 +8,7 @@ source "poky/oe-init-build-env"
 bitbake -c clean rgb-led
 "$script_dir/../../build.sh"
 pushd "$script_dir" || exit 1 # go to the 'modify' workspace
-make_command="$(grep -Pho -m1 '(?<=NOTE: )make .*$' $script_dir/../../build/tmp-glibc/work/raspberrypi3_64-oe-linux/rgb-led/1.0/temp/log.do_compile)"
+make_command="$(grep -Pho -m1 '(?<=NOTE: )make .*$' $script_dir/oe-logs/log.do_compile)"
 make_command="$(echo "$make_command" | head -n1)"
 # quote the CC arguments
 make_command="${make_command/aarch64-oe-linux-gcc//workspaces/final-project-bennowotny/build/tmp-glibc/work/x86_64-linux/gcc-cross-aarch64/13.3.0/sysroot-destdir/workspaces/final-project-bennowotny/build/tmp-glibc/work/x86_64-linux/gcc-cross-aarch64/13.3.0/recipe-sysroot-native/usr/bin/aarch64-oe-linux/aarch64-oe-linux-gcc}"
--- kernelspace/rgb-led/rgb-led.c
+++ kernelspace/rgb-led/rgb-led.c
@@ -6,15 +6,49 @@
  *
  *****************************************************************************/
 
+#include <linux/hrtimer.h>
+#include <linux/moduleparam.h>
 #include <linux/gpio.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/ktime.h>
 
 #define GPIO_R (514) // Header J8-3
 #define GPIO_G (515) // Header J8-5
 #define GPIO_B (516) // Header J8-7
 
+int dutyCycle = 0;
+module_param(dutyCycle, int, 0);
+
+long ns = 0;
+module_param(ns, long, 200);
+
+struct hrtimer pwmCycler;
+ktime_t expTime;
+
+enum hrtimer_restart on_timer(struct hrtimer* timer){
+  static uint8_t cycleCount = 0;
+
+  hrtimer_forward_now(timer, expTime);
+  
+  if(cycleCount == 0 && dutyCycle != 0){
+    gpio_set_value(GPIO_R, 1);
+    gpio_set_value(GPIO_G, 1);
+    gpio_set_value(GPIO_B, 1);
+  }else if(cycleCount == dutyCycle){
+    gpio_set_value(GPIO_R, 0);
+    gpio_set_value(GPIO_G, 0);
+    gpio_set_value(GPIO_B, 0);
+  }
+  
+  ++cycleCount;
+  return HRTIMER_RESTART;
+}
+
 static int __init rgb_led_init(void) {
   pr_info("Hello World!\n");
+  pr_info("Using duty cycle: %d\n", dutyCycle);
 
   if (gpio_request(GPIO_R, "RGB_R") < 0) {
     pr_err("ERROR: RGB R (%d) request\n", GPIO_R);
@@ -34,6 +68,11 @@ static int __init rgb_led_init(void) {
   }
   gpio_direction_output(GPIO_B, 1);
 
+  hrtimer_init(&pwmCycler, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+  pwmCycler.function = on_timer;
+  expTime = ktime_set(0, ns); // 162760, i.t.
+  hrtimer_start(&pwmCycler, expTime, HRTIMER_MODE_REL);
+
   return 0;
 
 dealloc_gpio_b:
@@ -46,6 +85,8 @@ dealloc_gpio_r:
 
 static void __exit rgb_led_exit(void) {
   pr_info("Goodbye Cruel World!\n");
+
+  hrtimer_cancel(&pwmCycler);
 
   gpio_set_value(GPIO_R, 0);
   gpio_set_value(GPIO_G, 0);
