1589fcaa55882caf11df9e1ecd560131a7c25374
Ben Nowotny <44074469+bennowotny@users.noreply.github.com>
2024-11-24 15:55:48 -0600

    #6: Implement PWM Bit-Banging (#20)
    * wip: parameterized the kernel module, testing a couple different configurations for the PWM output
    
    Signed-off-by: Ben Nowotny <bennowotny65535@gmail.com>
    
    * formatting changes, parameterize all 3 channels
    
    Signed-off-by: Ben Nowotny <bennowotny65535@gmail.com>
    
    ---------
    
    Signed-off-by: Ben Nowotny <bennowotny65535@gmail.com>

--- kernelspace/rgb-led/rgb-led.c
+++ kernelspace/rgb-led/rgb-led.c
@@ -1,38 +1,80 @@
-/******************************************************************************
- *
- *   Copyright (C) 2011  Intel Corporation. All rights reserved.
- *
- *   SPDX-License-Identifier: GPL-2.0-only
- *
- *****************************************************************************/
-
 #include <linux/gpio.h>
+#include <linux/hrtimer.h>
+#include <linux/kernel.h>
+#include <linux/ktime.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
 #define GPIO_R (514) // Header J8-3
 #define GPIO_G (515) // Header J8-5
 #define GPIO_B (516) // Header J8-7
 
+// load time parameter for the duty cycle
+int redDutyCycle = 0;
+module_param(redDutyCycle, int, 0);
+int greenDutyCycle = 0;
+module_param(greenDutyCycle, int, 0);
+int blueDutyCycle = 0;
+module_param(blueDutyCycle, int, 0);
+
+// load time parameter for the period of the PWM cycle
+long ns = 80000; // roughly 48Hz
+module_param(ns, long, 200);
+
+struct hrtimer pwmCycler;
+ktime_t expTime;
+
+enum hrtimer_restart on_timer(struct hrtimer *timer) {
+  static uint8_t cycleCount = 0;
+
+  hrtimer_forward_now(timer, expTime);
+
+  if (cycleCount == 0) {
+    if (redDutyCycle != 0)
+      gpio_set_value(GPIO_R, 1);
+    if (greenDutyCycle != 0)
+      gpio_set_value(GPIO_G, 1);
+    if (blueDutyCycle != 0)
+      gpio_set_value(GPIO_B, 1);
+  } else {
+    if (cycleCount == redDutyCycle)
+      gpio_set_value(GPIO_R, 0);
+    if (cycleCount == greenDutyCycle)
+      gpio_set_value(GPIO_G, 0);
+    if (cycleCount == blueDutyCycle)
+      gpio_set_value(GPIO_B, 0);
+  }
+
+  ++cycleCount;
+  return HRTIMER_RESTART;
+}
+
 static int __init rgb_led_init(void) {
-  pr_info("Hello World!\n");
+  pr_info("Using duty cycles: %d %d %d\n", redDutyCycle, greenDutyCycle, blueDutyCycle);
+  pr_info("Using period: %ld ns\n", ns);
 
   if (gpio_request(GPIO_R, "RGB_R") < 0) {
     pr_err("ERROR: RGB R (%d) request\n", GPIO_R);
     goto dealloc_gpio_r;
   }
-  gpio_direction_output(GPIO_R, 1);
+  gpio_direction_output(GPIO_R, 0);
 
   if (gpio_request(GPIO_G, "RGB_G") < 0) {
     pr_err("ERROR: RGB G (%d) request\n", GPIO_G);
     goto dealloc_gpio_g;
   }
-  gpio_direction_output(GPIO_G, 1);
+  gpio_direction_output(GPIO_G, 0);
 
   if (gpio_request(GPIO_B, "RGB_B") < 0) {
     pr_err("ERROR: RGB B (%d) request\n", GPIO_B);
     goto dealloc_gpio_b;
   }
-  gpio_direction_output(GPIO_B, 1);
+  gpio_direction_output(GPIO_B, 0);
+
+  hrtimer_init(&pwmCycler, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+  pwmCycler.function = on_timer;
+  expTime = ktime_set(0, ns);
+  hrtimer_start(&pwmCycler, expTime, HRTIMER_MODE_REL);
 
   return 0;
 
@@ -45,7 +87,7 @@ dealloc_gpio_r:
 }
 
 static void __exit rgb_led_exit(void) {
-  pr_info("Goodbye Cruel World!\n");
+  hrtimer_cancel(&pwmCycler);
 
   gpio_set_value(GPIO_R, 0);
   gpio_set_value(GPIO_G, 0);
